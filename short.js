4ec07666afb6cd5d41abe0e8e43d6abe176887fd
evoxalien <me@johnvenzon.com>
2019-04-30 18:01:50 -0700

    Updated short.js to accept JSON

--- short.js
+++ short.js
@@ -5,46 +5,53 @@ const urlWebsite = "http://www.short.com"
 const tableName = 'shortURL'
 //process.env.NAMEWHATEVER
 exports.handler = async function(event, context) {
-  if(event.body != null)
-  {
-    let data ={
-      shortURL: "",
-      originalURL: ""
-    }
-    
-    let response = {
-      statusCode: "",
-      body: ""
-    }
-
-    const original = event.body
-    const short = shortid.generate()
-    console.log(short)
-    console.log(typeof(short))
-    console.log(original)
-    console.log(typeof(original))
-    var params = {
-      Item: {
-        "shortuuid": {
-          S: short
-        },
-        "longurl": {
-          S: original
-        }
-      },
-      TableName: tableName
-    }
-
-    data.shortURL = short
-    data.originalURL = original
-
-    try {
-     await ddb.putItem(params).promise()
-      return { statusCode: 200, body: JSON.stringify(data) };
-    } 
-    catch(err) {
-      console.log(err)
-      return { statusCode: 500, body: JSON.stringify(err) };
-    }
+  if(event.body == null){
+    return { statusCode: 500, body: "Error: Try passing valid JSON"}
   }
+  const parsedInput = JSON.parse(event.body)
+  if(parsedInput.originalURL == undefined)
+  {
+    return { statusCode: 500, body: 'Error: Make sure your body contains JSON like this - {"originalURL":"google.com"}'}
+  }
+
+  let data ={
+    shortURL: "",
+    originalURL: ""
+  }
+  
+  let response = {
+    statusCode: "",
+    body: ""
+  }
+
+  const original = event.body
+  const short = shortid.generate()
+  console.log(short)
+  console.log(typeof(short))
+  console.log(original)
+  console.log(typeof(original))
+  var params = {
+    Item: {
+      "shortuuid": {
+        S: short
+      },
+      "longurl": {
+        S: original
+      }
+    },
+    TableName: tableName
+  }
+
+  data.shortURL = short
+  data.originalURL = original
+
+  try {
+    await ddb.putItem(params).promise()
+    return { statusCode: 200, body: JSON.stringify(data) };
+  } 
+  catch(err) {
+    console.log(err)
+    return { statusCode: 500, body: JSON.stringify(err) };
+  }
+
 }; 
